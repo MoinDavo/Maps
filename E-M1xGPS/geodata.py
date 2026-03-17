ba3dd1616da3ebd634a433be61b3d097e1a7ea26
MoinDavo <davo.frewin@gmail.com>
2026-03-17 16:42:17 +0100

    remove constructor overload

--- E-M1xGPS/geodata.py
+++ E-M1xGPS/geodata.py
@@ -5,26 +5,12 @@ class GeoDataPoint:
 		self.datetime = datetime
 		self.latitude = latitude
 		self.longitude = longitude
-		self.coordinate = tuple(latitude, longitude)
-
-	def __init__(self, datetime:dt, coordinate: tuple[float, float]):
-		self.datetime = datetime
-		self.latitude = coordinate[0]
-		self.longitude = coordinate[1]
-		self.coordinate = coordinate
+		self.coordinate = (latitude, longitude)
 	
 	
 class OlympusGeoDataPoint(GeoDataPoint):
 	def __init__(self, datetime:dt, latitude:float, longitude:float, time, compass, pressure, temperature, acceleration):
 		super().__init__(datetime, latitude, longitude)
-		self.time = time
-		self.compass = compass
-		self.pressure = pressure
-		self.temperature = temperature
-		self.acceleration = acceleration
-
-	def __init__(self, datetime:dt, coordinate: tuple[float, float], time, compass, pressure, temperature, acceleration):
-		super().__init__(datetime, coordinate)
 		self.time = time
 		self.compass = compass
 		self.pressure = pressure
